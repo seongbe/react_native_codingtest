e1ed1a87fc3775649412c20d5d4df1af4854060c
seongbeom <seongbeom@seongbeomui-MacBookAir.local>
2025-09-19 12:27:44 +0900

    admob초기화코드

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import mobileAds from 'react-native-google-mobile-ads';
 
 export default function App() {
+  // AdMob 초기화
+  React.useEffect(() => {
+    mobileAds()
+      .initialize()
+      .then(adapterStatuses => {
+        console.log('AdMob 초기화 완료', adapterStatuses);
+      });
+  }, []);
+
   return (
     <NavigationContainer>
       <BottomTabNavigator />
     </NavigationContainer>
   );
 }
-
-// ...existing code...
